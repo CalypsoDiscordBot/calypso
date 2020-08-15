ebf3aa84a03709715d8274bd2e63fa334afc2ac1
Alexmdz77 <alextab77165@gmail.com>
2020-08-16 01:43:30 +0200

    Edit Whois command

--- Commandes/whois.js
+++ Commandes/whois.js
@@ -1,38 +1,40 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const db = require('quick.db');
+const formatDate = require('dateformat');
+const { stripIndents } = require("common-tags");
 
 module.exports.run = (client, message, args) => {
 
     const member = message.mentions.members.first() || message.member;
     // if (!membre) { return message.channel.send('Veuillez mentionner un utilisateur !'); }
 
-    const joined = formatDate(member.joinedAt);
-        const roles = member.roles
+    const joined = formatDate(member.joinedAt, "m/d/yy");
+        const roles = member.roles.cache
             .filter(r => r.id !== message.guild.id)
             .map(r => r).join(", ") || 'none';
 
         // User variables
-        const created = formatDate(member.user.createdAt);
+        const created = formatDate(member.user.createdAt, "m/d/yy");
 
-        const embed = new RichEmbed()
+        const embed = new Discord.MessageEmbed()
             .setFooter(member.displayName, member.user.displayAvatarURL)
             .setThumbnail(member.user.displayAvatarURL())
             .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 
-            .addField('Member information:', stripIndents`**> Display name:** ${member.displayName}
-            **> Joined at:** ${joined}
-            **> Roles:** ${roles}`, true)
+            .addField('Member information:', `**▫️ Display name:** ${member.displayName}
+            **▫️ Joined at:** ${joined}
+            **▫️ Roles:** ${roles}`, true)
 
-            .addField('User information:', stripIndents`**> ID:** ${member.user.id}
-            **> Username**: ${member.user.username}
-            **> Tag**: ${member.user.tag}
-            **> Created at**: ${created}`, true)
+            .addField('User information:', `**▫️ ID:** ${member.user.id}
+            **▫️ Username**: ${member.user.username}
+            **▫️ Tag**: ${member.user.tag}
+            **▫️ Created at**: ${created}`, true)
             
             .setTimestamp()
 
         if (member.user.presence.game) 
-            embed.addField('Currently playing', stripIndents`**> Name:** ${member.user.presence.game.name}`);
+            embed.addField('Currently playing', `**▫️ Name:** ${member.user.presence.game.name}`);
 
         message.channel.send(embed);
 };
