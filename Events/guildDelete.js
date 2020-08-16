72bd27d2d3dc5212dd5a92b23d36107b1c81e1c0
Alexmdz77 <alextab77165@gmail.com>
2020-08-16 09:54:10 +0200

    Fix setactivity

--- Events/guildDelete.js
+++ Events/guildDelete.js
@@ -8,8 +8,19 @@ module.exports = (client, guild) => {
 			users = g.filter((member) => !member.user.bot).size;
 		});
 		count += users;
-		client.user.setActivity(`Développé par Alexmdz77 | Bêta | Serveurs : ${client.guilds.cache.size} | Users : ${count}`, { type: 'WATCHING' });
-	});
+		const statuts = [
+			'Calypso Bot | !help',
+			'Calypso v0.1 Bêta',
+			`${client.guilds.cache.size} Servers`,
+			`${count} Members`,
+    	];
+		console.log(`${count} Members ${client.channels.cache.size} Channels ${client.guilds.cache.size} Servers.`);
+		let i = 0;
+		setInterval(() => {
+			const statut = statuts[i++ % statuts.length];
+			client.user.setActivity(statut, { type: 'WATCHING' });
+		}, 10000);
+ 	});
 
 	const moi = client.users.cache.get('334786552964186123');
 
