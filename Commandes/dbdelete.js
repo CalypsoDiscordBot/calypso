02a3dfdcc76d6441713448fd8f076a5958858b04
Alexmdz77 <32090268+Alexmdz77@users.noreply.github.com>
2020-08-22 21:46:45 +0200

    Add Haz226

--- Commandes/dbdelete.js
+++ Commandes/dbdelete.js
@@ -1,15 +1,19 @@
 const Discord = require("discord.js");
+const db = require('quick.db');
 
 module.exports.run = (client, message, args) => {
     if (message.author.id !== '216607323035009025' && message.author.id !== '334786552964186123' && message.author.id !== '278608007535919115') { return; }
     else {
-        message.author.send('Arrêt du bot...')
-            .then(() => process.exit(0));
+        if(!args[0]){return;}
+        const dbname = db.fetch(args[0]);
+        if(!dbname){message.channel.send(":x: db not found")}
+        db.delete(args[0]);
+        message.channel.send("Success")
     }
 };
 
 module.exports.help = {
-    name: 'stopbot',
+    name: 'dbdelete',
     description: "",
     category: "admin",
     usage:"",
--- Commandes/reload.js
+++ Commandes/reload.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = (client, message, args) => {
-    if (message.author.id !== '216607323035009025' && message.author.id !== '334786552964186123') { return; }
+    if (message.author.id !== '216607323035009025' && message.author.id !== '334786552964186123' && message.author.id !== '278608007535919115') { return; }
     else {
         let commandName = args[0].toLowerCase();
         try{
