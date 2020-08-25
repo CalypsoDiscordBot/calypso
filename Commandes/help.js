49dc2d94366d2485031020bbba193141a2243ec8
Alexmdz77 <32090268+Alexmdz77@users.noreply.github.com>
2020-08-25 11:30:59 +0200

    Update help

--- Commandes/help.js
+++ Commandes/help.js
@@ -22,11 +22,11 @@ module.exports.run = (client, message, args) => {
           fields: [
            {
              name: ":wrench: Settings",
-             value: `\`${client.prefix}accessrole\`, \`${client.prefix}channeltoggle\`, \`${client.prefix}prefix\`, \`${client.prefix}configjoin\`, \`${client.prefix}configleave\`, \`${client.prefix}testjoin\`, \`${client.prefix}testleave\``
+             value: `\`${client.prefix}prefix\`, \`${client.prefix}configjoin\`, \`${client.prefix}configleave\`, \`${client.prefix}testjoin\`, \`${client.prefix}testleave\`, \`${client.prefix}autorole\`, \`${client.prefix}membercount\`, \`${client.prefix}accessrole\`, \`${client.prefix}channeltoggle\``
            },
            {
              name: ":hammer: Moderation",
-             value: `\`${client.prefix}clean\`, \`${client.prefix}ban\`, \`${client.prefix}unban\`, \`${client.prefix}kick\`, \`${client.prefix}poll\`, \`${client.prefix}giveaway\`, \`${client.prefix}reroll\`, \`${client.prefix}say\`, \`${client.prefix}embedsay\``
+             value: `\`${client.prefix}clean\`, \`${client.prefix}poll\`, \`${client.prefix}giveaway\`, \`${client.prefix}reroll\`, \`${client.prefix}say\`, \`${client.prefix}embedsay\``
            },
            {
              name: ":notes: Music",
