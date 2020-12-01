ef12dcb3dd023bde09ec5b760a9a71fe6b1b842f
Alexmdz77 <alext77165@gmail.com>
2020-12-01 14:58:19 +0100

    Add cat command

---
+++ Commandes/fun/cat.js
@@ -0,0 +1,26 @@
+const config = require('../../config.json');
+const request = require("request");
+
+module.exports.run = (__client, message) => {
+    request("https://aws.random.cat/meow", (error, res, body) => {
+        if (error) { return message.channel.send("Error-API"); }
+        if (res.statusCode !== 200) { 
+          return message.channel.send(`Error: ${res.statusCode}`); 
+        }
+        var body = JSON.parse(body);
+        message.channel.send({
+            embed: {
+                color: config.color,
+                image: { url: body.file }
+            }
+        })
+    });
+}
+module.exports.help = {
+    name: 'cat',
+    description: "Random cat picture.",
+    category: "fun",
+    usage:"",
+    accessableby: "Members",
+    aliases: []
+}
