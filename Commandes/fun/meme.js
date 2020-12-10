ae15b55bfb22e1924c7e44b397bc9d061ff8d64c
Alexmdz77 <32090268+Alexmdz77@users.noreply.github.com>
2020-12-10 23:17:04 +0100

    Change typing function

--- Commandes/fun/meme.js
+++ Commandes/fun/meme.js
@@ -1,7 +1,7 @@
 const randomPuppy = require('random-puppy');
 const config = require('../../config.json');
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client, message, args) => {
 
     let reddit = [
         "meme",
@@ -30,9 +30,9 @@ module.exports.run = async (bot, message, args) => {
                     color: client.color,
                     image: { url: url }
                 }
-            }).then(() => message.channel.stopTyping());
+            });
     }).catch(err => console.error(err));
-
+    message.channel.stopTyping();
 };
 
 module.exports.help = {
