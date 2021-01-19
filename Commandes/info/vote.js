0c1b37fcce4c8ee276393a7d30b930a6ff5a3716
Alexmdz77 <32090268+Alexmdz77@users.noreply.github.com>
2021-01-19 11:22:15 +0100

    Add Top.gg

--- Commandes/info/vote.js
+++ Commandes/info/vote.js
@@ -3,10 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = (client, message, args) => {
     var info = new Discord.MessageEmbed()
         .setTitle('Calypso | Vote')
-        .setDescription(`[Vote on discordbotlist.com !](https://discordbotlist.com/bots/calypso-3856/upvote)
-        [Vote on Discord Boats !](https://discord.boats/bot/740539000615469106/vote)
-        [Vote on discord-botlist.eu !](https://www.discord-botlist.eu/bots/740539000615469106/vote)
-        [Vote on Wonder Bot List !](https://wonderbotlist.com/fr/bot/740539000615469106/vote)`)
+        .setDescription(`[Vote on Top.gg !](https://top.gg/bot/740539000615469106/vote)\n[Vote on Discord Boats !](https://discord.boats/bot/740539000615469106/vote)\n[Vote on discord-botlist.eu !](https://www.discord-botlist.eu/bots/740539000615469106/vote)\n[Vote on Wonder Bot List !](https://wonderbotlist.com/fr/bot/740539000615469106/vote)`)
         .setFooter(`Calypso`)
         .setImage("")
         .setTimestamp()
