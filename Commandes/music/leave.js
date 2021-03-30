d26f5c88d24313a50af5cb989cd37be279ab2f84
Alexmdz77 <alext77165@gmail.com>
2021-03-30 12:41:25 +0200

    updated DMP to 7.0.0

--- Commandes/music/leave.js
+++ Commandes/music/leave.js
@@ -13,7 +13,7 @@ module.exports.run = (client, message, args) => {
 
     if(message.guild.me.voice.channel){
         if(message.member.voice.channel === message.guild.me.voice.channel){
-            client.player.stop(message.guild.id);
+            client.player.stop(message);
             message.guild.me.voice.channel.leave();
             message.react('✅');
         }
