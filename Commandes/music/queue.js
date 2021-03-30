e6baf4b2c4b02b1e7c1ced34f25dd6ea69a308ef
Alexmdz77 <alext77165@gmail.com>
2021-03-30 12:42:28 +0200

    New queue

--- Commandes/music/queue.js
+++ Commandes/music/queue.js
@@ -3,33 +3,44 @@ const config = require('../../config.json');
 
 module.exports.run = async (client, message, args) => {
 
+    const queueLength = 10;
 
     let queue = await client.player.getQueue(message);
-    let length = queue.songs.length;
-    if(queue.songs.length > 10){
-        length = 10;
-    }
     if(!queue || !queue.songs[0]) {
         const embed = new Discord.MessageEmbed()
             .setColor(client.color)
             .setDescription(message.language.music.error_notplaying())
         return message.channel.send(embed);
     }
+    const [current, ...next] = queue.songs;
+    // let length = queue.songs.length;
+    // if(queue.songs.length > 11){
+    //     length = 11;
+    // }
+    
 
-    let resp = message.language.queue.nowplaying(queue.songs[0].name, queue.songs[0].url, queue.songs[0].duration, queue.songs[0].requestedBy);
+    // let resp = message.language.queue.nowplaying(queue.songs[0].name, queue.songs[0].url, queue.songs[0].duration, queue.songs[0].requestedBy);
 
-    if(queue.songs[1]){
-        resp += message.language.queue.upnext();
-    }
-    for(var i = 1; i <= length; i++){
-        resp += message.language.queue.list(i, queue.songs[i].name, queue.songs[i].url, queue.songs[i].duration, queue.songs[i].requestedBy);
-    }
+    // if(queue.songs[1]){
+    //     resp += message.language.queue.upnext();
+    // }
+    // for(var i = 1; i < length; i++){
+    //     console.log(i);
+    //     resp += message.language.queue.list(i, current.name, current.url, current.duration, current.requestedBy);
+    // }
 
+    const string1 = message.language.queue.nowplaying(queue.songs[0].name, queue.songs[0].url, queue.songs[0].duration, queue.songs[0].requestedBy);
+        const string12 = next.length && message.language.queue.upnext();
+        /** Queue */
+        const string2 = next.length && next.slice(0,queueLength).map((song,i) => message.language.queue.list(i+1, song.name, song.url, song.duration, song.requestedBy)).join('');
+        /** Longer than queue... */
+        const string3 = next.length > queueLength && `and other ${next.length-queueLength} songs...`;    
+   
     message.channel.send({
         embed: {
             title: message.language.queue.title(message.guild.name),
             color: client.color,
-            description: resp
+            description: [string1,string12,string2,string3].filter(s=>s).join('')
         }}
     );
 };
